f2b563a06b9336d0597e27f52c7621749b298659
umeshsaravanan <umeshsaravanan13@gmail.com>
2025-03-20 22:37:07 +0530

    added react-icons

--- src/pages/ManageExpense.js
+++ src/pages/ManageExpense.js
@@ -1,4 +1,5 @@
 import React, { useState } from 'react'
+import { FaTimes } from 'react-icons/fa';
 
 const ManageExpense = () => {
     const [addClick, setAddClick] = useState(false);
@@ -7,8 +8,11 @@ const ManageExpense = () => {
         <div className="h-screen min-h-screen sm:p-0 bg-gradient-to-br from-[#256a63] to-[#029688]">
             <div className='flex flex-col justify-between h-full'>
                 <h1>your expense</h1>
-                <div className='bg-white min-h-[50px] width-full rounded-t-lg' onClick={() => setAddClick(true)}>
-                    <h3>add your expense</h3>
+                <div className='bg-white min-h-[50px] width-full rounded-t-lg p-3 cursor-pointer' onClick={() => setAddClick(true)}>
+                    <div className='flex gap-3 justify-between'>
+                        <h3>add your expense</h3>
+                        <FaTimes size={20} onClick={(e) => {e.stopPropagation() ;setAddClick(false)}} className='cursor-pointer'/>
+                    </div>
                     {
                         addClick && (
                             <>
